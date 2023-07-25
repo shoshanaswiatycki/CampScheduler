2812f2423e3e14abdab4a16da1c52eb9d434ba0e
Rivky <rivkyswiatycki@gmail.com>
2023-07-26 00:03:17 +0300

    add dapper

--- SQL/01 Tables.sql
+++ SQL/01 Tables.sql
@@ -1,6 +1,11 @@
+drop table if exists Location 
 create table Location(
     Id int identity primary key,
     Name varchar(50) not null,
     MaxBunkCount int not null,
     IsIndoor bit not null
 )
+
+
+
+
